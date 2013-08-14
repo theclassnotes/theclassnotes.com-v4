25c513c852a02b91fabfe65694e1334a6a59c0e4
Parker Moore <parkrmoore@gmail.com>
2013-08-14 04:21:14 +0200

    Guard-compass prefers to work from the base of the project ok.

--- _sass/config.rb
+++ _sass/config.rb
@@ -1,5 +1,5 @@
-sass_dir   = "./"
-css_dir    = "../css"
-images_dir = "../images"
+sass_dir   = "./_sass"
+css_dir    = "./css"
+images_dir = "./images"
 line_comments = false
 output_style = (environment.to_s == 'production') ? :compressed : :nested
