c4ae9dce8b1843a187daa7a6af2dd02eab45e6f9
tgb57 <tgb57@cornell.edu>
2016-09-20 16:36:09 -0400

    Please dont break

--- _sass/config.rb
+++
@@ -1,6 +0,0 @@
-require "susy"
-sass_dir   = "./_sass"
-css_dir    = "./css"
-images_dir = "./images"
-line_comments = false
-output_style = (environment.to_s == 'production') ? :compressed : :nested
